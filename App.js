26b9df77230eab3f17d4ae7945bade2a493854c9
jculver1 <jculver1900@gmail.com>
2019-01-20 20:16:14 -0700

    removed catch error for now

--- App.js
+++ App.js
@@ -1,39 +1,41 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text, View } from 'react-native';
+// import Error from './Components/error'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      foods: []
+      isLoading: true,
+      foods: [],
+      error: null,
+
     }
   }
-
   
-  componentWillMount(){ 
-    await fetch ('http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=g03EsNMIdLVGVFxer9G0rkguZEPyUf2dcDyxlKH6&nutrients=205&nutrients=204&nutrients=208&nutrients=269')
-  .then(function(response) {
-    return response.json()
-  })
-  .then(myJson => {
-    myJson.map(reports => reports.foods)
-      }
-    )
-    this.setState({
-      foods: myJson
+  componentDidMount(){ 
+    fetch('http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=g03EsNMIdLVGVFxer9G0rkguZEPyUf2dcDyxlKH6&nutrients=205&nutrients=204&nutrients=208&nutrients=269')
+      .then(res => res.json())
+      .then(myJson => {
+        myJson.map(food => {
+          this.setState({
+            foods: food,
+            isLoading: false 
+          })
     })
-  
-  }
+  })
+}
+
   addFood = () => {
     const NameFood = this.state.foods.map(food => food.name)
-    return NameFood
+    return NameFood[1]
   }
 
   render() {
     return (
       <View>
-        <text Some text />
-
+        {/* this.state.error ? <Error>Oh No!</Error> : <text addFood /> */}
+        <text>addFood</text>
         </View> 
     )
   }
