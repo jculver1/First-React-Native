0154439cc08b4d85f745866c470f137a6ffc223b
jculver1 <jculver1900@gmail.com>
2019-01-27 18:23:06 -0700

    added list component.

--- App.js
+++ App.js
@@ -1,43 +1,42 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, Text, View, FlatList } from 'react-native';
 // import Error from './Components/error'
 
-export default class App extends React.Component{
+ class App extends React.Component{
   constructor(props) {
     super(props)
     this.state = {
       isLoading: true,
       foods: [],
-      error: null,
+      error: null
 
     }
   }
-  
+
   componentDidMount(){ 
     fetch('http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=g03EsNMIdLVGVFxer9G0rkguZEPyUf2dcDyxlKH6&nutrients=205&nutrients=204&nutrients=208&nutrients=269')
-      .then(res => res.json())
+      .then(data => data.json())
       .then(myJson => {
-        myJson.map(food => {
           this.setState({
-            foods: food,
-            isLoading: false 
+            foods: myJson
           })
     })
-  })
 }
 
-  // addFood = () => {
-  //   const NameFood = this.state.foods.map(food => food.name)
-  //   return NameFood[1]
-  // }
+findFood = () => {
+  const mapFoods = this.state.foods.report.map(report =>report.food)
+  console.log(mapFoods)
+  return mapFoods
+}
 
   render() {
     return (
       <View>
-        {/* this.state.error ? <Error>Oh No!</Error> : <text addFood /> */}
         <Text>hello there</Text>
-        <Text>{this.state.foods[0]}</Text>
+        <Text>{this.findFood}</Text>
         </View> 
     )
   }
 }
+
+export default App
