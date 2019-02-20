9a663b9cf53bf857c731e3eb1fe30809003d9de7
jculver1 <jculver1900@gmail.com>
2019-02-20 13:44:40 -0700

    picker is displaying nutrient groups. need to reduce to one option each

--- App.js
+++ App.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import { AppRegistry, Text, View, FlatList, StyleSheet } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
 // import Error from './Components/error'
+import PickerExample from './components/picker'
 
  class App extends React.Component{
   constructor(props) {
     super(props)
     this.state = {
       isLoading: true,
-      error: null
+      error: null,
+      item: '',
+      data: [],
+      nutrientType: []
     }
-  }
+  }i
 
   componentDidMount(){ 
     fetch('https://api.nal.usda.gov/ndb/reports/?ndbno=01009&type=f&format=json&api_key=g03EsNMIdLVGVFxer9G0rkguZEPyUf2dcDyxlKH6&nutrients=205&nutrients=204&nutrients=208&nutrients=269')
@@ -23,28 +27,18 @@ import RNPickerSelect from 'react-native-picker-select';
     })
   }
   )
-  // console.log(this.state.items)
 }
 
+pickType = () => {
+  console.log('hello')
+}
 
   render() {
     return (
       <View>
         <Text style={styles.red}>hello there</Text>
         <Text>Choose something!</Text>
-        {/* <RNPickerSelect
-                    placeholder={{
-                        label: 'Select a color...',
-                        value: null,
-                        color: '#9EA0A4',
-                    }}
-                    items={this.state.items}
-                    onValueChange={(value) => {
-                        this.setState({
-                            favColor: value,
-                        });
-                    }}
-        /> */}
+        <PickerExample nutrientType={this.state.nutrientType} item={this.state.item} pickType={this.pickType}/>
         </View> 
     )
   }
---
+++ components/picker.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, Picker, StyleSheet } from 'react-native'
+
+const PickerExample = (props) => {
+    return(
+    <Picker
+        selectedValue={props.item}
+        onValueChange={() => props.pickType()}>{
+        props.nutrientType.map( (v, i)=>{
+        return <Picker.Item key={i} label={v} value={v} />
+        })
+        }
+    </Picker>
+    )}
+
+// const styles = StyleSheet.create({
+//    text: {
+//       fontSize: 30,
+//       alignSelf: 'center',
+//       color: 'red'
+//    }
+// })
+
+export default PickerExample
